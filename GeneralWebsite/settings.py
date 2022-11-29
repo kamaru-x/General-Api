606f93746432a667278b2163694ca9911a4a2be7
Kamaru <xin.kamaru@gmail.com>
2022-11-29 14:26:00 +0530

    static error fix

--- GeneralWebsite/settings.py
+++ GeneralWebsite/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR,'static/')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
