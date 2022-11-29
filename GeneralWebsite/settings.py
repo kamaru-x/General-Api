a67f3e4da620a8ad6f576a3aea32e24f17b308b0
Kamaru <xin.kamaru@gmail.com>
2022-11-29 14:59:26 +0530

    static error fix

--- GeneralWebsite/settings.py
+++ GeneralWebsite/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR,'static/')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = BASE_DIR / 'GeneralWebsite/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
